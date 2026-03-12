b87229cfa50c50539e43127ec0993461e82854e8
Ronny Mandal <ronnyma@transfinite.io>
2026-03-12 21:20:12 +0100

    Split into functions

--- src/main/scala/planesweep/model/Segment.scala
+++ src/main/scala/planesweep/model/Segment.scala
@@ -3,9 +3,16 @@ package planesweep.model
 
 case class Segment(p1: Point, p2: Point) {
 
+  def computeLineFraction(ys: Double): Double = {
+    (ys - p1.y) / (p2.y - p1.y)
+  }
+
   def lineIntersects(ys: Double): Boolean = {
-    val t = (ys - p1.y) / (p2.y - p1.y)
-    isInsideSegmentsYInterval(t)
+    isInsideSegmentsYInterval(computeLineFraction(ys: Double))
+  }
+
+  def xCoordinateGivenSweepline(ys: Double) = {
+    p1.x + computeLineFraction(ys: Double) * (p2.x - p1.x)
   }
 
   private def isInsideSegmentsYInterval(sweepLine: Double): Boolean =
